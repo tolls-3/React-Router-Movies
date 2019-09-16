9e43cab8b94f645f14c6f860b9a2e7af78d304ae
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-09-16 14:27:20 +0100

    Part1 tasks done

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import {Route} from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 import SavedList from './Movies/SavedList';
 
@@ -12,7 +15,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path ='/' component={MovieList}/>
+      <Route exact path ='/movie/:id' component={Movie}/>
     </div>
   );
 };
