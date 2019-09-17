96b7c775ee8fffa6d88180ad67f11257cdc45df2
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-09-17 16:14:47 +0100

    mvp acheived

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import React, { useState } from 'react';
-import {Route} from 'react-router-dom';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import SavedList from "./Movies/SavedList";
+import Movie from "./Movies/Movie";
+import MovieList from "./Movies/MovieList";
 
-import SavedList from './Movies/SavedList';
+
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <Route exact path ='/' component={MovieList}/>
-      <Route exact path ='/movie/:id' component={Movie}/>
-    </div>
+      <Route exact path="/" component={MovieList} />
+      <Route
+        path="/movies/:id"
+        render={props => <Movie {...props} addToSavedList={addToSavedList} />}
+      />
+    </div> 
   );
 };
 
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -3,29 +3,30 @@ import axios from 'axios';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState({});
- 
+  
   useEffect(() => {
     const id = props.match.params.id;
-  
+    // const movies = movie.find(movie => `${movie.id}`=== id);
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
-
-       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-
+    
+    axios
+    .get(`http://localhost:5000/api/movies/${id}`)
+    .then(response => {
+      setMovie(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+    
   },[props.match.params.id]);
+
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -43,14 +44,19 @@ const Movie = (props) => {
           Metascore: <strong>{metascore}</strong>
         </div>
         <h3>Actors</h3>
+        {stars ? stars.map(star => (
+				<div key={star} className="movie-star">
+					{star}
+				</div>
+			)) : null}
 
-        {stars.map(star => (
+        {/* {stars.map(star => (
           <div key={star} className="movie-star">
             {star}
           </div>
-        ))}
+        ))} */}
       </div>
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={saveMovie}>Save</div>
     </div>
   );
 }
